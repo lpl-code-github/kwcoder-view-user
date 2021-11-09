51dec8e987f53b657419cb3cc040b8a893dbaca2
李鹏龙 <lplgitee@user.noreply.gitee.com>
2021-11-09 18:10:04 +0800

    页面底部组件

---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<!-- 底部 -->
+<template>
+  <div class='footer'>
+		<div>
+		Copyright © 2021 实干青年 | <a href="http://www.beian.gov.cn/"  style="color:gray;">苏ICP备2021004789号</a>
+		</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Footer',
+  components: {},
+  data() {
+    return {
+
+    }
+  },
+  computed: {},
+  watch: {},
+  methods: {
+
+  },
+  created() {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+<style lang='less' scoped>
+.footer {
+  font-size: 14px;
+  height: 100%;
+}
+</style>
